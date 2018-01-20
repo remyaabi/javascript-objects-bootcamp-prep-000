caf70fa97ef5ebd3eb32f6364cd30b978f46e7dc
Remya Jacob <remyaabi@gmail.com>
2018-01-20 06:44:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist={abc:"abc1",
               def:"def1"
               }
+<<<<<<< HEAD
 var artistname="ghi";
 var  songtitle="ghi1";
 function updatePlaylist(playlist, artistname,songtitle){
@@ -10,5 +11,17 @@ function updatePlaylist(playlist, artistname,songtitle){
 
 function removeFromPlaylist(playlist, artistname){
   delete playlist[artistname];
+=======
+var artist=ghi;
+var song="ghi1";
+function updatePlaylist(playlist, artist,song){
+  playlist.artist=`song`;
+ // playlist[artist]=song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist;
+>>>>>>> 83b52c5019a991f683354e393110cb9274483cef
   return playlist;
 }
